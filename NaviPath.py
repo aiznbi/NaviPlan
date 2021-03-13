d74f7a4e058d446a8f618f50fe784dabed87c35e
aiznbi <79761018+aiznbi@users.noreply.github.com>
2021-03-13 17:07:11 +0300

    Add files via upload

---
+++ NaviPath.py
@@ -0,0 +1,64 @@
+from collections import deque
+from mailbox import mbox
+from tkinter import *
+import tkinter.messagebox as mb
+import json
+import self as self
+from PIL import ImageTk, Image
+import self as self
+from PIL.ImageWin import Window
+
+
+def bfs_paths(graph, start, goal):
+    queue = deque([(start, [start])])
+    while queue:
+        (vertex, path) = queue.pop()
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.appendleft((next, path + [next]))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+
+def in_out():
+    mb.askquestion(title="Выбор")
+
+
+tk = Tk()
+# загрузка img
+canvas = Canvas(tk, width=tk.winfo_screenwidth(), height=tk.winfo_screenheight(), bg='white')
+canvas.pack(expand=YES, fill=BOTH)
+self.image = ImageTk.PhotoImage(file="plan.gif")
+point_img = round(tk.winfo_screenwidth() / 2 - self.image.width() / 2)
+canvas.create_image(tk.winfo_screenwidth() / 2 - self.image.width() / 2, 0, image=self.image, anchor=NW)
+canvas.create_line(0, self.image.height(), tk.winfo_screenwidth(), self.image.height())
+with open("graph.json", "r") as read_file:
+    graph = json.load(read_file)
+with open("edges.json", "r") as read_file:
+    edges = json.load(read_file)
+with open("vertex.json", "r") as read_file:
+    vertex = json.load(read_file)
+for room in vertex:
+    Button(tk, text=room, command=in_out, font=("Times New Roman", 16)).place(x=vertex[room][0]-15 + point_img, y=vertex[room][1]-15)
+path = shortest_path(graph, "102", "103")
+way = []
+for i in path:
+    if edges.get(i) is None:
+        way.append(vertex.get(i))
+    else:
+        way.append(edges.get(i))
+for i in way:
+    i[0] += point_img
+canvas.create_oval(way[0][0]-20, way[0][1]-20, way[0][0]+20, way[0][1]+20, fill="green")
+canvas.create_oval(way[len(way)-1][0]-20, way[len(way)-1][1]-20, way[len(way)-1][0]+20, way[len(way)-1][1]+20, fill="red")
+for i in range(len(way)-1):
+    canvas.create_line(way[i], way[i+1], width=5, fill="blue")
+
+tk.mainloop()
---
+++ NaviPlan.py
@@ -0,0 +1,179 @@
+from tkinter import *
+import tkinter.messagebox as mb
+import json
+import self as self
+from PIL import ImageTk, Image
+
+graph = {}
+extender = []
+
+# лист пары координат точки
+cords = []
+# лист ребер для json
+edge = {}
+global i, j
+i = 0
+j = 0
+
+
+# бинд клика на коридор
+def add_new_hall():
+    ANH = mb.askokcancel(title="Коридор",
+                         message="Выберите точки напротив выходов из аудиторий, по окончании нажмите кнопку "
+                                 "'Завершить добавление коридора'")
+    if ANH:
+        canvas.bind("<Button-1>", on_click_hall)
+        global i, j
+        i = 0
+        j += 1
+    else:
+        canvas.bind("<Button-1>", NONE)
+
+
+# точки коридоров
+def on_click_hall(event):
+    mouse_x = canvas.winfo_pointerx() - canvas.winfo_rootx()
+    mouse_y = canvas.winfo_pointery() - canvas.winfo_rooty()
+    cords = [mouse_x - point_img, mouse_y]
+    global i, j
+    i += 1
+    edge["h" + str(j) + "_e" + str(i)] = cords
+    canvas.create_oval(mouse_x - 5, mouse_y - 5, mouse_x + 5, mouse_y + 5, fill="blue", width=2)
+
+
+# разбинд клика
+def exit_anh():
+    exitANH = mb.askokcancel(title="Операция завершена",
+                             message="Коридор добавлен")
+    canvas.bind("<Button-1>", NONE)
+
+
+# загрузка коридоров в json
+def load_halls():
+    with open("edges.json", "w") as write_file:
+        json.dump(edge, write_file)
+    exitHalls = mb.askokcancel(title="Операция завершена",
+                               message="Коридоры загружены")
+    if exitHalls:
+        # кнопки коридоров
+        for hall in edges:
+            Button(tk, text=hall, activebackground="blue", font=("Times New Roman", 14)).place(
+                x=edges[hall][0] + point_img - 15, y=edges[hall][1] - 15)
+        edge.clear()
+
+
+# бинд клика на аудиторию
+def add_new_room():
+    ANR = mb.askokcancel(title="Аудитория",
+                         message="Выберите аудиторию, затем смежные коридоры")
+    if ANR:
+        graph[room_name.get()] = []
+        canvas.bind("<Button-1>", on_click_room)
+    else:
+        canvas.bind("<Button-1>", NONE)
+
+
+# аудиторные вершины
+def on_click_room(event):
+    mouse_x = canvas.winfo_pointerx() - canvas.winfo_rootx()
+    mouse_y = canvas.winfo_pointery() - canvas.winfo_rooty()
+    cords = [mouse_x - point_img, mouse_y]
+    edge[txt.get()] = cords
+    canvas.create_oval(mouse_x - 10, mouse_y - 10, mouse_x + 10, mouse_y + 10, fill="red", width=2)
+
+
+# разбинд клика
+def exit_anr():
+    exitANH = mb.askokcancel(title="Операция завершена",
+                             message="Аудитория добавлена")
+    canvas.bind("<Button-1>", NONE)
+    room_name.set("")
+
+
+# загрузка аудиторий в json
+def load_rooms():
+    with open("vertex.json", "w") as write_file:
+        json.dump(edge, write_file)
+    exitHalls = mb.askokcancel(title="Операция завершена",
+                               message="Аудитории загружены")
+    with open("graph.json", 'w') as write_file:
+        json.dump(graph, write_file)
+    tk.destroy()
+
+
+# функция для коридора
+def addHall():
+    graph[hall_name.get()].append(room_name.get())
+    graph[room_name.get()].append(hall_name.get())
+    hall_name.set("")
+
+
+tk = Tk()
+# загрузка img
+canvas = Canvas(tk, width=tk.winfo_screenwidth(), height=tk.winfo_screenheight(), bg='white')
+canvas.pack(expand=YES, fill=BOTH)
+self.image = ImageTk.PhotoImage(file="plan.gif")
+point_img = round(tk.winfo_screenwidth() / 2 - self.image.width() / 2)
+canvas.create_image(tk.winfo_screenwidth() / 2 - self.image.width() / 2, 0, image=self.image, anchor=NW)
+canvas.create_line(0, self.image.height(), tk.winfo_screenwidth(), self.image.height())
+# кнопки для коридоров
+addNewHall = Button(tk, text="Добавить коридор", command=add_new_hall, font=("Times New Roman", 16))
+addNewHall.place(x=tk.winfo_screenwidth() / 4, y=self.image.height() + 2)
+addNewHallExit = Button(tk, text="Завершить добавление коридора", command=exit_anh, font=("Times New Roman", 16))
+addNewHallExit.place(x=tk.winfo_screenwidth() / 4, y=self.image.height() + 43)
+ExitHalls = Button(tk, text="Загрузить коридоры", command=load_halls, font=("Times New Roman", 16))
+ExitHalls.place(x=tk.winfo_screenwidth() / 4, y=self.image.height() + 84)
+
+# выгрузка коридоров
+with open("edges.json", "r") as read_file:
+    edges = json.load(read_file)
+
+# цикл смежности коридоров
+i = 1
+count = 0
+for hall in edges:
+    if int(hall[1:2]) == i:
+        extender.append(hall)
+        count += 1
+    else:
+        for x in range(count):  # по размеру списка
+            list1 = extender.copy()  # копия списка
+            list1.remove(extender[x])  # удалить одинаковый
+            graph[extender[x]] = list1
+            list1 = []  # очистка памяти и списка
+        extender.clear()
+        extender.append(hall)
+        i += 1
+        count = 1
+
+# костыль!
+for x in range(count):  # по размеру списка
+    list1 = extender.copy()  # копия списка
+    list1.remove(extender[x])  # удалить одинаковый
+    graph[extender[x]] = list1
+    list1 = []
+del list1
+extender = []
+# конец костыля!
+
+# кнопки для аудиторий
+room_name = StringVar()
+canvas.create_text(tk.winfo_screenwidth() / 2, self.image.height() + 10, text="Название аудитории",
+                   font=("Times New Roman", 16))
+txt = Entry(tk, width=15, font=("Times New Roman", 16), bd=3, bg="#bbbbbb", justify=CENTER, textvariable=room_name)
+txt.place(x=tk.winfo_screenwidth() / 2 - 85, y=self.image.height() + 25)
+addNewRoom = Button(tk, text="Добавить", command=add_new_room, font=("Times New Roman", 16))
+addNewRoom.place(x=tk.winfo_screenwidth() / 2 - 110, y=self.image.height() + 55)
+addNewRoomExit = Button(tk, text="Завершить", command=exit_anr, font=("Times New Roman", 16))
+addNewRoomExit.place(x=tk.winfo_screenwidth() / 2, y=self.image.height() + 55)
+ExitRooms = Button(tk, text="Загрузить аудитории", command=load_rooms, font=("Times New Roman", 16))
+ExitRooms.place(x=tk.winfo_screenwidth() / 2 - 98, y=self.image.height() + 95)
+# костыль для смежных коридоров
+hall_name = StringVar()
+canvas.create_text(tk.winfo_screenwidth() / 2 + 225, self.image.height() + 10, text="Смежный коридор",
+                   font=("Times New Roman", 16))
+text = Entry(tk, width=8, font=("Times New Roman", 16), bd=3, bg="#bbbbbb", justify=CENTER, textvariable=hall_name)
+text.place(x=tk.winfo_screenwidth() / 2 + 180, y=self.image.height() + 25)
+addHall = Button(tk, text="Добавить", command=addHall, font=("Times New Roman", 16))
+addHall.place(x=tk.winfo_screenwidth() / 2 + 175, y=self.image.height() + 55)
+tk.mainloop()
