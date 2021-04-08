031439c82467ddd13260bd17da90cd15dca417f3
aiznbi <boris.zuev05@yandex.ru>
2021-04-08 14:17:09 +0300

    Выпадающий список для аудиторий

--- NaviPath.py
+++ NaviPath.py
@@ -38,13 +38,27 @@ with open("edges.json", "r") as read_file:
     edges = json.load(read_file)
 with open("vertex.json", "r") as read_file:
     vertex = json.load(read_file)
+# лист для выпадающего списка
+OptList_from = []
+OptList_where = []
 # создание кнопок
 for room in vertex:
     Button(tk, text=room, font=("Times New Roman", 16)).place(x=vertex[room][0] - 15 + point_img,
                                                               y=vertex[room][1] - 15)
-from_but = Button(tk, text="Отсюда", font=("Times New Roman", 16))
-from_but.place(x=tk.winfo_screenwidth() / 4 + tk.winfo_screenwidth() / 8, y=image.height() + 5)
-path = shortest_path(graph, "102", "103")
+    OptList_from.append(room)
+    OptList_where.append(room)
+var_from = StringVar(tk)
+var_from.set("Откуда")
+opt_from = OptionMenu(tk, var_from, *OptList_from)
+opt_from.config(width=15, font=('Times New Roman', 20))
+opt_from.update()
+opt_from.place(x=tk.winfo_screenwidth()/2-opt_from.winfo_reqwidth(), y=image.height() + 5)
+var_where = StringVar(tk)
+var_where.set("Куда")
+opt_where = OptionMenu(tk, var_where, *OptList_where)
+opt_where.config(width=15, font=('Times New Roman', 20))
+opt_where.place(x=tk.winfo_screenwidth()/2, y=image.height() + 5)
+"""path = shortest_path(graph, "102", "103")
 way = []
 for i in path:
     if edges.get(i) is None:
@@ -58,5 +72,5 @@ canvas.create_oval(way[len(way) - 1][0] - 20, way[len(way) - 1][1] - 20, way[len
                    way[len(way) - 1][1] + 20, fill="red")
 for i in range(len(way) - 1):
     canvas.create_line(way[i], way[i + 1], width=5, fill="blue")
-
+"""
 tk.mainloop()
